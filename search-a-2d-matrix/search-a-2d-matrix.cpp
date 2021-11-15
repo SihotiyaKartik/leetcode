ae4c43e31bf3fcfcac976d428d8fb7d14245a52c
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2021-11-15 23:21:59 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (51.09%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0;
+        int high = m*n -1;
+        while(low <= high){
+            int mid = low + (high - low )/2;
+            int mid_v = matrix[mid/n][mid%n];
+            if(mid_v == target){
+                return true;
+            }
+            else if(mid_v > target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return false;
+        
+        
+    }
+};
